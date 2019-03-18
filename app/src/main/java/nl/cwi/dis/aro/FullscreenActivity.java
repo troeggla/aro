f599992921ffaff095b66227e307fa7269e15421
Thomas Röggla <t.roggla@cwi.nl>
2019-03-18 15:05:56 +0100

    cleaned up FullscreenActivity

--- app/src/main/java/nl/cwi/dis/aro/FullscreenActivity.java
+++ app/src/main/java/nl/cwi/dis/aro/FullscreenActivity.java
@@ -19,6 +19,7 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.util.Log;
 import android.content.pm.ActivityInfo;
+
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
@@ -45,28 +46,32 @@ public class FullscreenActivity extends AppCompatActivity {
     private View mContentView;
     private View mControlsView;
     private boolean mVisible;
-    private Button btn1;
+
     private EditText user_name_txt;
     private EditText user_age_txt;
     private RadioGroup user_gender_group;
-    private int videoID;
-    private String user_gender="";
-    private String annotation="valence,arousal\n";
+
+    private int videoID = 0;
+
+    private String user_gender = "";
+    private String annotation = "valence,arousal\n";
+
     private static final String LOG_TAG = "FullscreenActivity";
     private static final int WRITE_EXTERNAL_STORAGE_PERMISSION_REQUEST = 44;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
         if(getRequestedOrientation()!=ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE){
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         }
-        super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_fullscreen);
 
         mVisible = true;
         mControlsView = findViewById(R.id.fullscreen_content_controls);
         mContentView = findViewById(R.id.fullscreen_content);
-
 
         // Set up the user interaction to manually show or hide the system UI.
         mContentView.setOnClickListener(new View.OnClickListener() {
@@ -75,44 +80,47 @@ public class FullscreenActivity extends AppCompatActivity {
                 toggle();
             }
         });
-        videoID = 0;
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
+
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.WRITE_EXTERNAL_STORAGE }, WRITE_EXTERNAL_STORAGE_PERMISSION_REQUEST);
         } else {
             Log.d(LOG_TAG, "Permission already granted");
         }
+
         findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
-        btn1 = (Button)findViewById(R.id.start_button);
+
         user_age_txt = findViewById(R.id.age_txt);
         user_name_txt = findViewById(R.id.name_txt);
         user_gender_group = findViewById(R.id.gender_group);
+
+        Button btn1 = findViewById(R.id.start_button);
+
         btn1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 Intent i = new Intent(FullscreenActivity.this , VideoPlayer.class);
+
                 String user_name = user_name_txt.getText().toString().trim();
                 String user_age = user_age_txt.getText().toString().trim();
-                if(user_gender_group.getCheckedRadioButtonId()!=-1)
-                {
+
+                if (user_gender_group.getCheckedRadioButtonId() != -1) {
                     RadioButton rb = findViewById(user_gender_group.getCheckedRadioButtonId());
                     user_gender = rb.getText().toString();
                 }
-                if (user_name.length()==0)
-                    {Toast.makeText(FullscreenActivity.this,"Pleas input your name!", Toast.LENGTH_LONG).show();}
-                else if (user_age.length()==0)
-                    {Toast.makeText(FullscreenActivity.this,"Pleas input your age!", Toast.LENGTH_LONG).show();}
-                else if (user_gender.length()==0)
-                {Toast.makeText(FullscreenActivity.this,"Pleas select your gender!", Toast.LENGTH_LONG).show();}
-                else
-                {
-                    i.putExtra("annotation",annotation);
-                    i.putExtra("user_name",user_name);
-                    i.putExtra("user_age",user_age);
-                    i.putExtra("user_gender",user_gender);
+
+                if (user_name.length() == 0) {
+                    Toast.makeText(FullscreenActivity.this,"Please input your name!", Toast.LENGTH_LONG).show();
+                } else if (user_age.length() == 0) {
+                    Toast.makeText(FullscreenActivity.this,"Please input your age!", Toast.LENGTH_LONG).show();
+                } else if (user_gender.length() == 0) {
+                    Toast.makeText(FullscreenActivity.this,"Please select your gender!", Toast.LENGTH_LONG).show();
+                } else {
+                    i.putExtra("annotation", annotation);
+                    i.putExtra("user_name", user_name);
+                    i.putExtra("user_age", user_age);
+                    i.putExtra("user_gender", user_gender);
                     i.putExtra("videoID", videoID);
+
                     startActivity(i);
                 }
             }
