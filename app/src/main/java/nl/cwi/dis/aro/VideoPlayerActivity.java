e2cc5336a871d6b27c9950d1bf9da72d453987f0
Thomas Röggla <t.roggla@cwi.nl>
2019-03-22 13:44:55 +0100

    simplified hiding of system UI in VideoPlayerActivity

--- app/src/main/java/nl/cwi/dis/aro/VideoPlayerActivity.java
+++ app/src/main/java/nl/cwi/dis/aro/VideoPlayerActivity.java
@@ -1,13 +1,11 @@
 package nl.cwi.dis.aro;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Environment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
 
 import java.io.File;
 import java.util.Locale;
@@ -27,18 +25,8 @@ import java.util.TimerTask;
 import nl.cwi.dis.aro.extras.UserSession;
 import nl.cwi.dis.aro.views.MyRockerView;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
 public class VideoPlayerActivity extends AppCompatActivity {
-    /**
-     * Some older devices needs a small delay between UI widget updates
-     * and a change of the status and navigation bar.
-     */
-    private static final int UI_ANIMATION_DELAY = 300;
-    private final Handler mHideHandler = new Handler();
-    private View mContentView;
+    private static final String LOG_TAG = "VideoPlayerActivity";
 
     private double angle = 0;
     private double level = 0;
@@ -52,52 +40,13 @@ public class VideoPlayerActivity extends AppCompatActivity {
     private ImageView back_ground;
     private MyRockerView mRockerViewXY;
 
-    private static final String LOG_TAG = "VideoPlayerActivity";
-
-    private final Runnable mHidePart2Runnable = new Runnable() {
-        @SuppressLint("InlinedApi")
-        @Override
-        public void run() {
-            // Delayed removal of status and navigation bar
-
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        }
-    };
-    private View mControlsView;
-    private final Runnable mShowPart2Runnable = new Runnable() {
-        @Override
-        public void run() {
-            // Delayed display of UI elements
-            ActionBar actionBar = getSupportActionBar();
-            if (actionBar != null) {
-                actionBar.show();
-            }
-            mControlsView.setVisibility(View.VISIBLE);
-        }
-    };
-    private final Runnable mHideRunnable = this::hide;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_video_player);
 
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content);
-
         mRockerViewXY = findViewById(R.id.rockerXY_View);
-
-        VideoView videoView = findViewById(R.id.videoView);
-        videoView.setOnClickListener(view -> hide());
+        this.hideSystemUI();
     }
 
     public void onResume(){
@@ -276,31 +225,18 @@ public class VideoPlayerActivity extends AppCompatActivity {
         });
     }
 
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide();
-    }
-
-    private void hide() {
-        // Hide UI first
+    private void hideSystemUI() {
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.hide();
         }
-        mControlsView.setVisibility(View.GONE);
 
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowPart2Runnable);
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    private void delayedHide() {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, 100);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 
     private int dpToPx(Context context, float dp){
